c4f42a0f5ddf2d045a62667dea31209bc837deea
Pankaj Randhe <pankajrandhe@gmail.com>
2015-01-25 23:11:35 +0530

    Implemented the Locks for concurrency

--- assignment1/server/server.go
+++ assignment1/server/server.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"sync"
 )
 
 // Lets first create the structure which will hold the value and all other meta-data for the key
@@ -18,12 +19,17 @@ type value_and_metadata struct {
 }
 
 // Create the MAP to hold the keys, values and the associated meta-data
-var key_values map[string]*value_and_metadata
+//var key_values map[string]*value_and_metadata
+
+var kvmap = struct{
+    sync.RWMutex
+    key_values map[string]*value_and_metadata
+}{key_values: make(map[string]*value_and_metadata)}
 
 func main() {
 
 	//var key_values map[string] *value_and_metadata
-	key_values = make(map[string]*value_and_metadata)
+	//key_values = make(map[string]*value_and_metadata)
 
 	//create the TCP address to listen on
 	tcpAddress, err := net.ResolveTCPAddr("tcp", ":9000")
@@ -48,7 +54,7 @@ func main() {
 		}
 
 		// Now handle the connection
-		go handleConnection(connection, key_values)
+		go handleConnection(connection, kvmap.key_values)
 		defer connection.Close()
 	}
 }
@@ -84,6 +90,7 @@ func handleConnection(connection net.Conn, key_values map[string]*value_and_meta
 			var val string
 			var set_noreply string
 
+			//Generate the random number as the version
 			var ver int64 = rand.Int63()
 
 			arg1, err = strconv.ParseInt(command_split[2], 10, 64)
@@ -110,48 +117,64 @@ func handleConnection(connection net.Conn, key_values map[string]*value_and_meta
 
 			instance := value_and_metadata{val, arg1, arg2, ver} //val
 			ref_instance := &(instance)
-			key_values[string(command_split[1])] = ref_instance
+
+			kvmap.Lock()
+			kvmap.key_values[string(command_split[1])] = ref_instance
+			kvmap.Unlock()
 
 			if set_noreply != "noreply" {
-
-				fmt.Println(set_noreply)
 				_, err = connection.Write([]byte("OK " + strconv.FormatInt(key_values[string(command_split[1])].version, 10) + "\r\n")) //buffer[0:length_r])
-				fmt.Println("replied")
 				handleError(err)
-			}
+			} 
 
-			if key_values[string(command_split[1])].exptime != 0 {
+			kvmap.RLock()
+			if kvmap.key_values[string(command_split[1])].exptime != 0 {
+				kvmap.RUnlock()
 				//start the timer taking the KEY as an argument
-				go exp_timer(string(command_split[1]), key_values)
+				go exp_timer(string(command_split[1]), kvmap.key_values)
+			} else{
+				kvmap.RUnlock()
 			}
 
 		case "get":
+
 			arg1 := strings.Trim(command_split[1], "\r\n")
-			_, presence := key_values[arg1]
+			kvmap.RLock()
+			_, presence := kvmap.key_values[arg1]
 
 			if presence {
-				instance := key_values[arg1]
-				//_, err = connection.Write([]byte("VALUE 10 val#one"))
+
+				//acquire the lock for reading the key-value only, once we get the Reference release it
+				
+				instance := kvmap.key_values[arg1]
+				kvmap.RUnlock()
+
 				_, err = connection.Write([]byte("VALUE " + instance.numbytes + "\r\n" + instance.value + "\r\n"))
 				if false {
 					fmt.Println("VALUE " + instance.numbytes + "\r\n" + instance.value + "\r\n")
 				}
 				handleError(err)
 			} else {
+				kvmap.RUnlock()
 				_, err := connection.Write([]byte("ERRNOTFOUND\r\n"))
 				handleError(err)
 			}
 
 		case "getm":
+
 			arg1 := strings.Trim(command_split[1], "\r\n")
-			_, presence := key_values[arg1]
+			kvmap.RLock()
+			_, presence := kvmap.key_values[arg1]
 
 			if presence {
-				instance := key_values[arg1]
+
+				instance := kvmap.key_values[arg1]
 				_, err = connection.Write([]byte("VALUE " + strconv.FormatInt(instance.version, 10) + " " + strconv.FormatInt(instance.exptime, 10) + " " + instance.numbytes + "\r\n" + instance.value + "\r\n"))
+				kvmap.RUnlock()
 
 			} else {
 
+				kvmap.RUnlock()
 				_, err := connection.Write([]byte("ERRNOTFOUND\r\n"))
 				handleError(err)
 			}
@@ -188,36 +211,52 @@ func handleConnection(connection net.Conn, key_values map[string]*value_and_meta
 
 			//val = strings.Trim(string(buffer1[0:val_length]), "\r\n")
 
-			if arg1 == key_values[string(command_split[1])].version {
+			kvmap.RLock()
+			if arg1 == kvmap.key_values[string(command_split[1])].version {
 
+				kvmap.RUnlock()
+				kvmap.Lock()
 				// Update the old values
-				key_values[string(command_split[1])].exptime = arg2
-				key_values[string(command_split[1])].numbytes = arg3
-				key_values[string(command_split[1])].version = arg1
-				key_values[string(command_split[1])].value = val
+				kvmap.key_values[string(command_split[1])].exptime = arg2
+				kvmap.key_values[string(command_split[1])].numbytes = arg3
+				kvmap.key_values[string(command_split[1])].version = arg1
+				kvmap.key_values[string(command_split[1])].value = val
+				kvmap.Unlock()
 
 				if cas_noreply != "noreply" {
-					_, err = connection.Write([]byte("OK " + strconv.FormatInt(key_values[string(command_split[1])].version, 10) + "\r\n")) //buffer[0:length_r])
+					kvmap.RLock()
+					_, err = connection.Write([]byte("OK " + strconv.FormatInt(kvmap.key_values[string(command_split[1])].version, 10) + "\r\n")) //buffer[0:length_r])
+					kvmap.RUnlock()
 					handleError(err)
 				}
 
-				if key_values[string(command_split[1])].exptime != 0 {
+				kvmap.RLock()
+				if kvmap.key_values[string(command_split[1])].exptime != 0 {
 					//start the timer taking the KEY as an argument
+					kvmap.RUnlock()
 					go exp_timer(string(command_split[1]), key_values)
+				}else{
+					kvmap.RUnlock()
 				}
+			} else{
+				kvmap.RUnlock()
 			}
 
 		case "delete":
-			fmt.Println("Command is: DELETE")
-
 			arg1 := strings.Trim(command_split[1], "\r\n")
 			fmt.Println(arg1)
 
 			// lets use the Comma-OK function (value, present = m[key]) to delete the key-value pair
-			_, presence := key_values[arg1]
+			kvmap.RLock()
+			_, presence := kvmap.key_values[arg1]
+			kvmap.RUnlock()
+
 			if presence {
 				//map[arg1] = 1, false
-				delete(key_values, arg1)
+				kvmap.Lock()
+				delete(kvmap.key_values, arg1)
+				kvmap.Unlock()
+				
 				_, err = connection.Write([]byte("DELETED\r\n"))
 				handleError(err)
 			} else {
