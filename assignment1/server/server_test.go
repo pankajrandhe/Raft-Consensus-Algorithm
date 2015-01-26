e239d8c663d720ea9189faf2aac0e01e8c1e4bc3
Pankaj Randhe <pankajrandhe@gmail.com>
2015-01-26 12:27:28 +0530

    Added Test cases for the concurrent clients

--- assignment1/server/server_test.go
+++ assignment1/server/server_test.go
@@ -7,21 +7,48 @@ import (
 	"regexp"
 	"testing"
 	"time"
+	"errors"
 )
 
-func TestSet(t *testing.T) {
+func ClientTest(t *testing.T) {
 
-	go main()
 	time.Sleep(time.Duration(1) * time.Second)
+
+	var eCh error
+
+	//Test for the single client
+	eCh = setClient()
+
+	//Test 10 concurrent clients
+	for i:=0; i<10; i++{
+
+		go func(){
+			eCh = setClient()
+		}() 
+	}
+	if eCh!= nil{
+		t.Error(eCh)
+	}
+	var input string
+    fmt.Scanln(&input)
+    
+
+	
+}
+
+func setClient() (eChr error) {
 
 	connection, err1 := net.Dial("tcp", ":9000")
 	handleErr(err1)
 
 	ch := make(chan []byte)
 	eCh := make(chan error)
+	//eChr = make(chan error)
 	var data []byte
-	var match_err error
+	//var match_err error
 	var matched bool
+
+	err := errors.New("not matched")
 
 	// Start a goroutine to read from our net connection
 	go func(ch chan []byte, eCh chan error) {
@@ -37,47 +64,49 @@ func TestSet(t *testing.T) {
 			// send data if we read some.
 			ch <- data
 		}
-	}(ch, eCh)
+	}(ch,eCh)
 
 	//testcase#1 SET command
 	sendToServer(connection, "set one 0 10\r\nval#one\r\n")
 	data = <-ch
-	matched, match_err = regexp.MatchString("OK.*", string(data))
+	fmt.Println(string(data))
+	matched,_ = regexp.MatchString("OK.*", string(data))
 	if !matched {
-		t.Error(match_err)
+		fmt.Println(err)
+		return err
 	}
 
 	//testcase#2 SET command
 	sendToServer(connection, "set two 0 10\r\nval#two\r\n")
 	data = <-ch
-	matched, match_err = regexp.MatchString("OK.*", string(data))
+	fmt.Println(string(data))
+	matched,_ = regexp.MatchString("OK.*", string(data))
 	if !matched {
-		t.Error(match_err)
+		fmt.Println(err)
+		return err
 	}
 
 	//testcase#3 GET command
 	sendToServer(connection, "get one\r\n")
 	data = <-ch
-	matched, match_err = regexp.MatchString("VALUE 10\r\nval#one\r\n", string(data))
+	fmt.Println(string(data))
+	matched,_ = regexp.MatchString("VALUE 10\r\nval#one\r\n", string(data))
 	if !matched {
-		t.Error(match_err)
+		fmt.Println(err)
+		return err
 	}
 
 	//testcase#4 GETM command
 	sendToServer(connection, "getm one\r\n")
 	data = <-ch
-	matched, match_err = regexp.MatchString("VALUE ([0-9]*) 0 10\r\nval#one\r\n", string(data))
+	fmt.Println(string(data))
+	matched,_ = regexp.MatchString("VALUE ([0-9]*) 0 10\r\nval#one\r\n", string(data))
 	if !matched {
-		t.Error(match_err)
+		fmt.Println(err)
+		return err
 	}
 
-	//testcase#5 DELETE command
-	sendToServer(connection, "delete two\r\n")
-	data = <-ch
-	matched, match_err = regexp.MatchString("DELETED\r\n", string(data))
-	if !matched {
-		t.Error(match_err)
-	}
+	return nil
 }
 
 func handleErr(err error) {
@@ -91,3 +120,5 @@ func sendToServer(connection net.Conn, message string) {
 	_, err3 := connection.Write([]byte(message))
 	handleErr(err3)
 }
+
+		  
