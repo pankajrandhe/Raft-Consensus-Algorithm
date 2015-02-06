e73007f7c5c29fc1090f96052f8fa4501b536e52
pankajrandhe <pankajrandhe@gmail.com>
2015-02-06 12:07:25 +0530

    assignment2

---
+++ assignment2/raft/raft.go
@@ -0,0 +1,151 @@
+package raft
+
+import(
+	"os"
+	"errors"
+	"strconv"
+	"net"
+)
+
+type bytearray []byte
+
+type Lsn uint64  //Log sequence number, unique for all time.
+
+type ErrRedirect int  // See Log.Append. Implements Error interface.
+
+type LogEntry interface {
+	LSN() Lsn
+	Data() []byte
+	Committed() bool
+}
+
+func (x bytearray) LSN() Lsn { 
+
+	seqno := 10	//temporary
+	return Lsn(seqno)
+}
+
+func (x bytearray) Data() []byte {
+
+	return x
+}
+
+func (x bytearray) Committed() bool{
+
+	return true
+}
+
+type SharedLog interface {
+	// Each data item is wrapped in a LogEntry with a unique
+	// lsn. The only error that will be returned is ErrRedirect,
+	// to indicate the server id of the leader. Append initiates
+	// a local disk write and a broadcast to the other replicas,
+	// and returns without waiting for the result.
+	Append(data []byte) (LogEntry, error)
+}
+
+// --------------------------------------
+// Raft setup
+type ServerConfig struct {
+	Id int // Id of server. Must be unique
+	Hostname string // name or ip of host
+	ClientPort int 	// port at which server listens to client messages.
+	LogPort int	// tcp port for inter-replica protocol messages.
+}
+
+type ClusterConfig struct {
+	Path string 	// Directory for persistent log
+	Servers []ServerConfig	// All servers in this cluster
+}
+
+// Raft implements the SharedLog interface.
+type Raft struct {
+
+	cluster *ClusterConfig
+	thisServerId int
+	commitCh chan LogEntry
+}
+
+// Creates a raft object. This implements the SharedLog interface.
+// commitCh is the channel that the kvstore waits on for committed messages.
+// When the process starts, the local disk log is read and all committed
+// entries are recovered and replayed
+func NewRaft(config *ClusterConfig, thisServerId int, commitCh chan LogEntry) (*Raft, error) {
+
+	raft := Raft{config,thisServerId,commitCh}
+
+	var err error = nil
+
+	return &raft, err
+}
+
+// ErrRedirect as an Error object
+func (e ErrRedirect) Error() string {
+	return "Redirect to server " + strconv.Itoa(10)
+}
+
+func (raft Raft) Append(data []byte) (LogEntry, error){
+
+	// first we have to check if the server is the leader
+	var serverid int = 1 //dummy
+	var leaderid int = 1 //dummy
+
+	err := errors.New(string(leaderid))
+	
+	if serverid != leaderid{
+		return nil,err
+	} else {
+ 
+		/*
+		* Write the received log entry - data byte to a file in the local disk 
+		*
+		* References:
+		* Writing to Files - https://gobyexample.com/writing-files
+		* Appending to Files - http://stackoverflow.com/questions/7151261/append-to-a-file-in-go?lq=1
+		* Check whether file already exists - http://stackoverflow.com/questions/12518876/how-to-check-if-a-file-exists-in-go
+		*/
+
+		filename := (raft.cluster).Path
+
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			_, err := os.Create(filename)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		logFile, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0666)
+		if err != nil {
+		    panic(err)
+		}
+
+		defer logFile.Close()
+
+		if _, err = logFile.WriteString("\n"+string(data)); err != nil {
+		    panic(err)
+		}
+
+		// Take the raft object and broadcat the logentry
+		// Lets send the logentry to each of the servers in the cluster
+
+		//Read back the servers as JSON objects
+		servers := raft.cluster.Servers
+
+		for _, server := range servers{
+
+			host := server.Hostname
+			port := server.LogPort
+
+			//now establish the TCP connection and send the data to the server
+			connection, err1 := net.Dial("tcp", host+":"+strconv.Itoa(port))
+			panic(err1)
+
+			_, err3 := connection.Write(data)
+			panic(err3)
+
+		}
+
+		// Prepare the log entry and return it else error
+		return bytearray(data),nil
+	}
+}
---
+++ assignment2/raft_server/raft_server.go
@@ -0,0 +1,391 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/pankajrandhe/cs733/assignment2/raft"
+)
+
+// Lets first create the structure which will hold the value and all other meta-data for the key
+type value_and_metadata struct {
+	value    string
+	exptime  int64
+	numbytes string
+	version  int64
+}
+
+// Create the MAP to hold the keys, values and the associated meta-data
+var kvmap = struct {
+	sync.RWMutex
+	key_values map[string]*value_and_metadata
+}{key_values: make(map[string]*value_and_metadata)}
+
+func main() {
+
+	//First of all let the server know what is the Raft configuration by calling the NewRaft()
+
+	//read the json file and get the server configurations 
+
+	var cluster_config raft.ClusterConfig
+	server_configs, err := ioutil.ReadFile("/home/pankaj/go/src/github.com/pankajrandhe/cs733/assignment2/servers_json.json")
+	if err != nil{
+		panic(err)
+	}
+
+	err_json := json.Unmarshal(server_configs, &cluster_config)
+	if err_json != nil{
+		panic(err_json)
+	} 
+
+	serverId := 0 //dummy
+	commitCh := make(chan raft.LogEntry)
+
+	var raft_instance *(raft.Raft)
+	raft_instance, err = raft.NewRaft(&cluster_config, serverId, commitCh)
+
+	cluster_config = raft_instance.cluster	//dummy
+	//fmt.Println(raft.cluster)
+
+	//create the TCP address to listen on
+	tcpAddress, err := net.ResolveTCPAddr("tcp", ":9000")
+	handleError(err)
+
+	//now create the listener to listen on the above tcp address
+	listener, err := net.ListenTCP("tcp", tcpAddress)
+	handleError(err)
+
+	//keep listening for the client request
+	for {
+
+		//if the request arrives accept the request from the client and create the connection object
+		connection, err1 := listener.Accept()
+
+		if err1 != nil {
+			handleError(err1)
+			continue
+		}
+
+		// Now handle the connection
+		go handleConnection(connection, kvmap.key_values)
+		defer connection.Close()
+	}
+}
+
+func handleConnection(connection net.Conn, key_values map[string]*value_and_metadata) {
+
+	buffer := make([]byte, 512)
+	buffer_temp := make([]byte, 512)
+	var err error
+	var ver int64 = 1
+
+	for {
+
+		var client_command string
+		var command_split []string
+		var case_parameter string
+		var length_r, length_temp int
+		var newline_count int = 0
+
+		// Read the first packet to decide the course of the action
+		length_r, err = connection.Read(buffer[0:])
+		handleError(err)
+
+		newline_count = strings.Count(string(buffer[0:length_r]), "\r\n")
+
+		case_parameter = strings.Split(string(buffer[0:length_r]), " ")[0]
+
+		// check whether if we have got \r\n and command is not set or cas command
+		if strings.Contains(string(buffer[0:length_r]), "\r\n") && !(case_parameter == "set" || case_parameter == "cas") {
+
+			client_command = string(buffer[0:length_r])
+
+		} else if !(strings.Contains(string(buffer[0:length_r]), "\r\n")) && !(case_parameter == "set" || case_parameter == "cas") {
+
+			// In this case read the packets until we get the "\r\n"
+			for {
+				length_temp, err = connection.Read(buffer_temp[0:])
+				handleError(err)
+
+				buffer = append(buffer[0:length_r], buffer_temp[0:length_temp]...)
+				length_r = length_r + length_temp
+
+				if strings.Contains(string(buffer), "\r\n") {
+					break
+				} else {
+					continue
+				}
+			}
+			client_command = string(buffer[0:length_r])
+		} else {
+
+			if newline_count == 2 {
+				client_command = string(buffer[0:length_r])
+
+			} else {
+
+				// Otherwise go on reading until we get two "\r\n" characters
+				for {
+
+					if newline_count < 2 {
+						buffer_temp := make([]byte, 1024)
+						length_temp, err = connection.Read(buffer_temp[0:])
+						handleError(err)
+
+						buffer = append(buffer[0:length_r], buffer_temp[0:length_temp]...)
+						length_r = length_r + length_temp
+
+						if strings.Contains(string(buffer), "\r\n") {
+							newline_count++
+						}
+					} else {
+						break
+					}
+				}
+				client_command = string(buffer[0:length_r])
+			}
+		}
+
+		command_split = strings.Split(client_command, " ")
+
+		switch case_parameter {
+
+		case "set":
+			handleSet(command_split, connection, ver)
+
+		case "get":
+			handleGet(command_split, connection)
+
+		case "getm":
+			handleGetm(command_split, connection)
+
+		case "cas":
+			handleCas(command_split, connection, ver)
+
+		case "delete":
+			handleDelete(command_split, connection)
+
+		default:
+			_, err = connection.Write([]byte("ERRCMDERR \r\n"))
+			handleError(err)
+		}
+
+		if err != nil {
+			handleError(err)
+			return
+		}
+
+	}
+}
+
+func handleSet(command_split []string, connection net.Conn, ver int64) {
+	var arg1 int64
+	var arg2 string
+	var val string
+	var set_noreply string
+	var err error
+
+	//Generate the version
+	ver = ver + 1
+	arg1, err = strconv.ParseInt(command_split[2], 10, 64)
+	handleError(err)
+
+	if strings.Contains(command_split[3], "\r\n") {
+		arg := strings.Trim(command_split[3], "\r\n")
+		arg2 = (strings.Split(arg, "\r\n"))[0]
+		val = (strings.Split(arg, "\r\n"))[1]
+
+	} else {
+
+		arg2 = command_split[3] //(strings.Split(arg, "\r\n"))[0]
+		arg := strings.Trim(command_split[4], "\r\n")
+		set_noreply = (strings.Split(arg, "\r\n"))[0]
+		val = (strings.Split(arg, "\r\n"))[1]
+	}
+
+	instance := value_and_metadata{val, arg1, arg2, ver}
+	ref_instance := &(instance)
+
+	kvmap.Lock()
+	kvmap.key_values[string(command_split[1])] = ref_instance
+	kvmap.Unlock()
+
+	if set_noreply != "noreply" {
+
+		kvmap.RLock()
+		ver := strconv.FormatInt(kvmap.key_values[string(command_split[1])].version, 10)
+		kvmap.RUnlock()
+		_, err = connection.Write([]byte("OK " +ver+ "\r\n")) //buffer[0:length_r])
+		handleError(err)
+	}
+
+	kvmap.RLock()
+	if kvmap.key_values[string(command_split[1])].exptime != 0 {
+		kvmap.RUnlock()
+		//start the timer taking the KEY as an argument
+		go exp_timer(string(command_split[1]), kvmap.key_values)
+	} else {
+		kvmap.RUnlock()
+	}
+}
+
+func handleGet(command_split []string, connection net.Conn) {
+
+	var err error
+	arg1 := strings.Trim(command_split[1], "\r\n")
+
+	kvmap.RLock()
+	_, presence := kvmap.key_values[arg1]
+
+	if presence {
+
+		//acquire the lock for reading the key-value only, once we get the Reference release it
+		instance := kvmap.key_values[arg1]
+		kvmap.RUnlock()
+
+		_, err = connection.Write([]byte("VALUE " + instance.numbytes + "\r\n" + instance.value + "\r\n"))
+		handleError(err)
+	} else {
+		kvmap.RUnlock()
+		_, err := connection.Write([]byte("ERRNOTFOUND\r\n"))
+		handleError(err)
+	}
+}
+
+func handleGetm(command_split []string, connection net.Conn) {
+
+	var err error
+	arg1 := strings.Trim(command_split[1], "\r\n")
+	kvmap.RLock()
+	_, presence := kvmap.key_values[arg1]
+
+	if presence {
+
+		instance := kvmap.key_values[arg1]
+		_, err = connection.Write([]byte("VALUE " + strconv.FormatInt(instance.version, 10) + " " + strconv.FormatInt(instance.exptime, 10) + " " + instance.numbytes + "\r\n" + instance.value + "\r\n"))
+		kvmap.RUnlock()
+
+	} else {
+
+		kvmap.RUnlock()
+		_, err = connection.Write([]byte("ERRNOTFOUND\r\n"))
+		handleError(err)
+	}
+}
+
+func handleCas(command_split []string, connection net.Conn, ver int64) {
+
+	var arg1 int64 //version to be provided
+	var arg2 int64
+	var arg3 string
+	var val string
+	var cas_noreply string
+	var err error
+
+	arg1, err = strconv.ParseInt(command_split[2], 10, 64)
+	handleError(err)
+
+	arg2, err = strconv.ParseInt(command_split[3], 10, 64)
+	handleError(err)
+
+	if strings.Contains(command_split[4], "\r\n") {
+		arg := strings.Trim(command_split[4], "\r\n")
+		arg3 = (strings.Split(arg, "\r\n"))[0]
+		val = (strings.Split(arg, "\r\n"))[1]
+
+	} else {
+
+		arg3 = strings.Trim(command_split[4], " ")
+		cas_noreply = strings.Trim(command_split[5], "\r\n")
+
+	}
+
+	kvmap.RLock()
+	if arg1 == kvmap.key_values[string(command_split[1])].version {
+
+		kvmap.RUnlock()
+		kvmap.Lock()
+		// Update the old values
+		kvmap.key_values[string(command_split[1])].exptime = arg2
+		kvmap.key_values[string(command_split[1])].numbytes = arg3
+		kvmap.key_values[string(command_split[1])].version = arg1
+		kvmap.key_values[string(command_split[1])].value = val
+		kvmap.Unlock()
+
+		if cas_noreply != "noreply" {
+			kvmap.RLock()
+			_, err = connection.Write([]byte("OK " + strconv.FormatInt(kvmap.key_values[string(command_split[1])].version, 10) + "\r\n")) //buffer[0:length_r])
+			kvmap.RUnlock()
+			handleError(err)
+		}
+
+		kvmap.RLock()
+		if kvmap.key_values[string(command_split[1])].exptime != 0 {
+			//start the timer taking the KEY as an argument
+			kvmap.RUnlock()
+			go exp_timer(string(command_split[1]), kvmap.key_values)
+		} else {
+			kvmap.RUnlock()
+		}
+	} else {
+
+		_, err = connection.Write([]byte("ERR_VERSION\r\n"))
+		handleError(err)
+		kvmap.RUnlock()
+	}
+
+}
+
+func handleDelete(command_split []string, connection net.Conn) {
+
+	var err error
+	arg1 := strings.Trim(command_split[1], "\r\n")
+
+	kvmap.RLock()
+	_, presence := kvmap.key_values[arg1]
+	kvmap.RUnlock()
+
+	if presence {
+		kvmap.Lock()
+		delete(kvmap.key_values, arg1)
+		kvmap.Unlock()
+
+		_, err = connection.Write([]byte("DELETED\r\n"))
+		handleError(err)
+	} else {
+
+		_, err = connection.Write([]byte("ERRNOTFOUND\r\n"))
+		handleError(err)
+	}
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func exp_timer(key string, key_values map[string]*value_and_metadata) {
+
+	ticker := time.NewTicker(time.Second * 1)
+	record := key_values[key]
+	time_limit := record.exptime
+
+	go func() {
+
+		for range ticker.C {
+
+			record.exptime = record.exptime - 1
+		}
+	}()
+	time.Sleep(time.Duration(time_limit) * time.Second)
+	ticker.Stop()
+	// as soon as the timer expires delete the record
+	delete(key_values, key)
+}
