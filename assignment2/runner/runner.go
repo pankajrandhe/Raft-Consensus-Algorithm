b3b61f9045108c82955ec7d47bb3b9b9ca8c6b5c
pankajrandhe <pankajrandhe@gmail.com>
2015-02-08 17:43:31 +0530

    Executing server in their own process and logentry return

---
+++ assignment2/runner/runner.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+	"fmt"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+
+	path, err1 := exec.LookPath("raft_server")
+	if err1 != nil {
+		log.Fatal("Check your PATH variable")
+	}
+
+		wg.Add(1)
+		go func(){
+
+			cmd := exec.Command(path, "0")
+			cmd.Stdout = os.Stdout
+		    cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {panic(err)}
+			defer wg.Done()
+		}()
+		
+		wg.Add(1)
+		go func(){
+
+			cmd := exec.Command(path, "1")
+			cmd.Stdout = os.Stdout
+		    cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {panic(err)}
+			defer wg.Done()
+		}()
+
+		wg.Add(1)
+		go func(){
+			cmd := exec.Command(path, "2")
+			cmd.Stdout = os.Stdout
+		    cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			fmt.Println("In runner")
+			if err != nil {panic(err)}
+			defer wg.Done()
+		}()
+		
+	wg.Wait()	
+	//cmd.Wait()
+}
+
+	
