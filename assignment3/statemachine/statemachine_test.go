bf821c5af3dd7e1990deb65507d7f13b9b6fec2a
pankajrandhe <pankajrandhe@gmail.com>
2015-03-31 00:58:27 +0530

    added test for leader change

--- assignment3/statemachine/statemachine_test.go
+++ assignment3/statemachine/statemachine_test.go
@@ -21,7 +21,7 @@ func TestRaft(t *testing.T) {
 	var commitCount1, commitCount2 int = 0, 0
 	var response1, response2 raft.LogStruct
 	
-	// check who is the leader
+	// check who is the current leader
 	for{
 		for s:=0; s<5;s++{
 			leader = raft.RaftMap[s].LeaderId  //check if any of the server has info. about the leader
@@ -34,13 +34,15 @@ func TestRaft(t *testing.T) {
 			break
 		}
 	}
-	// Leader is legitimate or not (there should be only one leader OR leader HB not yet received)
+	// Leader is legitimate or not (there should be only one leader OR HB not yet received i.e. -1 status)
 	for s:=0; s<5;s++{
 		if (leader == raft.RaftMap[s].LeaderId) || (raft.RaftMap[s].LeaderId == -1){
 		} else{
 			t.Error("More than one leader for the term")
 		}
 	}
+
+	/***********************/
 	
 	// Now Append at leader's log and check if the logentry pops out at each server's commit channel
 	raft.RaftMap[leader].ClientSend("set country India\r\n")
@@ -58,6 +60,8 @@ func TestRaft(t *testing.T) {
 		t.Error("Logentry not coming out of the commit channel of the majority servers")
 	}
 	
+	/***********************/
+
 	// Send another Append and check if logs are ordered
 	raft.RaftMap[leader].ClientSend("set country Sweden\r\n")
 	time.Sleep(3*time.Second)
@@ -77,4 +81,29 @@ func TestRaft(t *testing.T) {
 		t.Error("Logentries are not monotonically increasing")
 	}
 
+	/***********************/
+
+	// Now bring down the original leader and check if another leader is elected
+	raft.RaftMap[leader].EventCh <- raft.KillServer{}
+	time.Sleep(2*time.Second)
+	// check who is the leader
+	for{
+		for s:=0; s<5;s++{
+			leader = raft.RaftMap[s].LeaderId  //check if any of the server has info. about the leader
+			if leader != -1{
+				flag = true
+				break
+			}
+		}
+		if flag{
+			break
+		}
+	}
+	// New Leader should be elected or not (there should be only one leader OR HB not yet received i.e. -1 status)
+	for s:=0; s<5;s++{
+		if (leader == raft.RaftMap[s].LeaderId) || (raft.RaftMap[s].LeaderId == -1){
+		} else{
+			t.Error(raft.RaftMap[s].LeaderId)
+		}
+	}
 }
